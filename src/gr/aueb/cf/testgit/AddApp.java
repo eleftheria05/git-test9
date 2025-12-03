380fc9a342273094d90441ef8204e90e927c4fdc
Eleftheria Sfetkou <eleftheriasfetkou@gmail.com>
2025-12-03 22:26:12 +0200

    feat: add new class for addition

---
+++ src/gr/aueb/cf/testgit/AddApp.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.testgit;
+
+import java.sql.SQLOutput;
+
+public class AddApp {
+    static void main() {
+        System.out.println(add(1, 2));
+    }
+
+    public static int add(int a, int b){
+        return a+b;
+    }
+}
